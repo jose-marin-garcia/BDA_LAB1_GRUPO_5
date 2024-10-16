dcea4cc46759d2f9609f0bcba8050383d9869e86
jose-marin-garcia <jose.marin.g@usach.cl>
2024-10-16 12:59:21 -0300

    Backend

---
+++ BackendLab1/src/main/java/Laboratorio1BdaGrupo5/BackendLab1/BackendLab1Application.java
@@ -0,0 +1,13 @@
+package Laboratorio1BdaGrupo5.BackendLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLab1Application.class, args);
+	}
+
+}
---
+++ BackendLab1/src/test/java/Laboratorio1BdaGrupo5/BackendLab1/BackendLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Laboratorio1BdaGrupo5.BackendLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
