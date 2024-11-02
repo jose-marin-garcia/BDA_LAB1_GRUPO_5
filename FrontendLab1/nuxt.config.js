7f2dfefe4b5491d3f0e299050381f8e8fd01a8b6
jose-marin-garcia <jose.marin.g@usach.cl>
2024-11-02 00:49:54 -0300

    Agrega axios en frontend

--- FrontendLab1/nuxt.config.js
+++ FrontendLab1/nuxt.config.js
@@ -43,5 +43,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+  baseURL: 'http://localhost:3000',
   }
 }
