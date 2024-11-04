c8d7b8900f4e25f308b0e76a68a1fb990581d56c
Vitoko9123 <victor.manuel.duarte.arce@gmail.com>
2024-11-04 15:56:45 -0300

    Crud de categoria

---
+++ BackendLab1/src/main/java/Laboratorio1BdaGrupo5/BackendLab1/models/Categoria.java
@@ -0,0 +1,13 @@
+package Laboratorio1BdaGrupo5.BackendLab1.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    private Integer id_categoria;
+    private String nombre;
+}
--- BackendLab1/src/main/java/Laboratorio1BdaGrupo5/BackendLab1/repository/ClienteRepository.java
+++ BackendLab1/src/main/java/Laboratorio1BdaGrupo5/BackendLab1/repository/ClienteRepository.java
@@ -12,4 +12,6 @@ public interface ClienteRepository {
 
     void deleteCliente(Integer idCliente);
 
+
+
 }
